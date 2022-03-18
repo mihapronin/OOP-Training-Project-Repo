80a8ec037786e76330223191e4af278784feb333
mihapronin <mikhail.pronin@gmail.com>
2022-03-18 15:31:28 +0300

    Basik animal behavior
    Animal is controlled by Player. Speed is different for the different animals

--- OOP Training Project/Assets/Scripts/Animal.cs
+++ OOP Training Project/Assets/Scripts/Animal.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool AnimalIsOnScene;
+    public float speed;
+    private float horzontalInput;
+    private float verticalInput;
+    private bool controlledByPlayer = true;
+
+    private void Update()
     {
-        
+        horzontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        if (controlledByPlayer)
+        {
+            ControlByPlayer();
+        }
+        else
+        {
+            FoodIsBad();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ControlByPlayer()
     {
-        
+        Move(horzontalInput, verticalInput);
+    }
+
+    public void Move(float horizontalMove, float verticalMove)
+    {
+        gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalMove);
+        gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalMove);
+    }
+
+    public virtual void FoodIsGood()
+    {
+
+    }
+
+    public virtual void FoodIsBad()
+    {
+        Move(0.5f, 0.5f);
     }
 }
